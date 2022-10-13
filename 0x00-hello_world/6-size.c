803428a3aa7f67505f69d00e5b8794070b64646e
samiaosama <samiaosama.soomaosama1998@gmail.com>
2022-10-13 15:47:06 -0700

    0-preprocessor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * the computer it is compiled  
  * Return: always (0)
  */
-
 int main(void)
 {
 	char acharacter;
@@ -18,6 +16,5 @@ printf("Size of an int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
-
 return (0);
 }
