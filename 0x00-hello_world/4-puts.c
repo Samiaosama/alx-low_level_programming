70b5f9ce804293cb75db00e5159c12e5ed65c5c3
samiaosama <samiaosama.soomaosama1998@gmail.com>
2022-10-13 13:17:54 -0700

    0-preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,23 @@
-#include <stdio.h>
-/**
- * main -Entry point
- *
- * Return: always 0
- */
-int main (void)
+<include <stdio.h#
+
+**/
+
+main - Entry point *
+
+*
+*
+* Return: always 0 *
+*
+*/*
+
+int main(void)
+
+	}
+
+puts("\"Programming is like building a
+
+		;multilingual puzzle")
+
+;return (0)
+
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
-}
