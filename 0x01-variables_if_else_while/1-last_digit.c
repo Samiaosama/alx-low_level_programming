56309ae78a3d454feb5963a86dd22a28ed2e1ca9
samiaosama <samiaosama.soomaosama1998@gmail.com>
2022-10-14 15:05:02 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,8 +7,8 @@
  *  number stored in the variable n
  *  Return: Always 0 (Success)
  * /
- * int main(void)
- * {
+  int main(void)
+  {
                int n;
 
                srand(time(0));
@@ -20,7 +20,7 @@
 	       printf("and is 0\n");
 	    else if (n % 10 == 0)
 	       printf("and is 0\n";
-	    else if (n % 10 < 6 && n % 10 != 0)
+	    else if (n % 10 < 6 && n % 10 != 0);
 	       printf("and is less than 6 and not 0\n");
 	       return (0);
 	       }
