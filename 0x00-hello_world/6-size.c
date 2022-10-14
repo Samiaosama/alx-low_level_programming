c1ce35f858f70d2351a767407aaf2cf21e93f8f4
samiaosama <samiaosama.soomaosama1998@gmail.com>
2022-10-14 03:35:13 -0700

    0-preprocessor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *
- * main - Entry point
+ *main-Entry point
  *
  * Return: Always 0 (Success)
  */ 
-int main(void)
+intmain(void)
 {
 	char acharacter;
 	int ainteger;
