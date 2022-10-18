8a103ba4448452e9c443a5cdbe690c5cb376e0da
samiaosama <samiaosama.soomaosama1998@gmail.com>
2022-10-18 07:40:16 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,6 +26,7 @@ void times_table(void)
 					_putchar ((c % 10) + '0');
 				}
 				if (b < 9)
+				{
 					_putchar(',');
 				_putchar (' ');
 			}
@@ -42,5 +43,4 @@ void times_table(void)
 		}
 	}
 	_putchar ('\n');
-}
 }
