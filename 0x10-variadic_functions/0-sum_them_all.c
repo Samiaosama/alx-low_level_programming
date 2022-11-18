3e93637fa44b7e6c0cc02df977c05a2b3c6ecad6
samiaosama <samiaosama.soomaosama1998@gmail.com>
2022-11-17 18:17:47 -0800

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,11 @@
 #include "variadic_functions.h"
 
+/**
+ * sum_them_all - adds all the numbers
+ * @n: the number of parameters passed
+ * Return: Always 0
+ */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum;
