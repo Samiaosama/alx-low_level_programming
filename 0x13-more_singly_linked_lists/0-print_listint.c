1d01b5d4fc80d6ce66e8f2f7209a98b52b65e8e2
samiaosama <samiaosama.soomaosama1998@gmail.com>
2022-11-28 05:02:19 -0800

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * Print_listint(const listint_t *h);
+ *
+ * @h: head of linklist node
+ * Return: the number of nodes
+ *
+ */
+size_t print_listint(const listint_t *h);
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+				h = h->next;
+						count++;
+	}
+	return (count);
+}
