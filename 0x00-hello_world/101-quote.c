35dc8da19459ee6f3834c34a1e40aee9371641f0
root <soomaosama1998@gamil.com>
2024-02-23 09:19:14 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("and that piece of art is useiful");
+	puts("Dora Korpar");
+	puts("2015-10-19");
+	return (1);
+}
