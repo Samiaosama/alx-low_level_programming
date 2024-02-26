62aa7f469bb378438aee2331506e27fd45cf8079
root <soomaosama1998@gamil.com>
2024-02-26 09:14:19 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - check the code
@@ -7,6 +7,10 @@
  */
 int main(void)
 {
-	ptintf("_putchar\n");
+	char str [] = "_putchar";
+	int ch;
+	for(ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar(\n);
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h>
+
+/**
+ * putchar - writes the charcterc to stdout
+ *
+ * return : one success 1.
+ *
+ */
+int _putchar (char c)
+{
+	return(write (1,&c,1);
+			}
